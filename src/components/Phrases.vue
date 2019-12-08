f213bc1046a9ee452c47b0226a5af53faf96f056
Maxime Colin <contact@maximecolin.fr>
2019-12-08 17:05:03 +0100

    first version

---
+++ src/components/Phrases.vue
@@ -0,0 +1,84 @@
+<script>
+import Draggable from 'vuedraggable'
+
+export default {
+  components: {
+    Draggable,
+  },
+  data() {
+    return {
+      phrases: [
+        [],
+        [],
+        [],
+      ],
+      words: [
+        'I', 'You', 'He', 'She',
+        'was', 'am', 'are',
+        'blue', 'tall', 'short',
+      ],
+    }
+  },
+  methods: {
+    addPhrase() {
+      this.phrases.push([])
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <div class="phrases">
+      <div v-for="(phrase, index) in phrases" :key="index">
+        Phrase #{{index}}
+        <draggable v-model="phrases[index]" group="words" class="phrase">
+          <span class="word" v-for="(phraseWord, w) in phrase" :key="w">{{phraseWord}}</span>
+        </draggable>
+      </div>
+      <button type="button" @click="addPhrase()">Add</button>
+    </div>
+    <div class="words">
+      <draggable v-model="words" group="words">
+        <span class="word" v-for="(word, w) in words" :key="w">{{word}}</span>
+      </draggable>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  * {
+    box-sizing: border-box;
+  }
+
+  .container {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .phrases {
+    width: 70%;
+    padding: 20px;
+    max-height: 100vh;
+    overflow: scroll;
+  }
+
+  .words {
+    padding: 20px;
+  }
+
+  .word {
+    background: #eee;
+    border: 1px solid #ccc;
+    display: inline-block;
+    padding: 5px 10px;
+    margin: 3px;
+  }
+
+  .phrase {
+    padding: 10px;
+    border: 1px dashed #eee;
+    min-height: 62px;
+    margin-bottom: 10px;
+  }
+</style>
